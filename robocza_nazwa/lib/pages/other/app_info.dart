51d4d4bb1680e9763648e2b3d472a0f9de135f44
Kacper Daniel <kacperd258@gmail.com>
2023-08-07 19:41:19 +0200

    added const keyword

--- robocza_nazwa/lib/pages/other/app_info.dart
+++ robocza_nazwa/lib/pages/other/app_info.dart
@@ -5,7 +5,7 @@ class AppInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text("App version: 0.0.1", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),),
